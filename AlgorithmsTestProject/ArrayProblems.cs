abc9a5238c88c951658458a2bca00da3f7146f1e
Robin Debry <91249812+robin-debry@users.noreply.github.com>
2023-05-24 13:56:01 +0200

    Update ArrayProblems.cs

--- AlgorithmsTestProject/ArrayProblems.cs
+++ AlgorithmsTestProject/ArrayProblems.cs
@@ -2,52 +2,72 @@ namespace AlgorithmsTestProject;
 
 public static class ArrayProblems
 {
+    private static object T;
+
     public static bool AreArraysEqual<T>(T[] xs, T[] ys)
     {
-        throw new NotImplementedException();
+        return xs.SequenceEqual(ys);
+       
     }
 
     public static void Swap<T>(T[] xs, int a, int b)
     {
+
+        T temp = xs[a];
         xs[a] = xs[b];
-        xs[b] = xs[a];
+        xs[b] = temp;
+        
+        
+
     }
 
     public static T FirstElement<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        return xs.First();
     }
 
     public static T LastElement<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        return xs.Last();
     }
 
     public static T MiddleElement<T>(T[] xs)
     {
+
         throw new NotImplementedException();
     }
 
     public static void Reverse<T>(T[] xs)
     {
-        throw new NotImplementedException();
+      
     }
 
     public static int CountElement<T>(T[] xs, T element)
-    { 
-        throw new NotImplementedException();
+    {
+       return Count(xs, x => x.Equals(element));
     }
 
     public static string ToCommaDelimitedString<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        var r = "";
+        for (var i=0; i<xs.Length;i++)
+        {
+            if (i>0)
+            {
+                r += ",";
+                r += xs[i];
+            }
+            
+        }
+        return r;
     }
 
     // Bonus problems
 
     public static int Count<T>(T[] xs, Func<T, bool> predicate)
     {
-        throw new NotImplementedException();
+
+        return xs.Count();
     }
 
     public static T Min<T>(T[] xs, Func<T, T, int> comparer)
